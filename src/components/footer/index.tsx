aba2d394d8cf83024865626df816d6a07892ac28
Binit <binitkumar0911@gmail.com>
2023-11-12 10:15:16 +0530

    deploy

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
             Lorem vitae ut augue auctor faucibus eget eget ut libero. Elementum purus et arcu massa dictum condimentum.
             Augue scelerisque iaculis orci ut habitant laoreet. Iaculis tristique.
           </p>
-          <p>© Evogym All Rights Reserved.</p>
+          <p>©2023 Made with 💖 Binitech.</p>
         </div>
         <div className="mt-16 basis-1/4 md:mt-0">
           <h4 className="font-bold">Links</h4>
